5a825d4c7916d7cd0a35ec8bcec3dfe5ca9f8818
dandakltd <dilshansanjeewaperera@gmail.com>
2022-08-25 16:08:02 -0400

    Python

---
+++ Everything in the course.py
@@ -0,0 +1,5 @@
+#print ("One,'two', \"three\", four,\n\\five\\once, 'I' caught a \n\tfish'//alive\\\\'" )
+x = 5
+y = 12
+z = x + y
+print(z)
---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 19 01:51:39 2022
+
+@author: dilsh
+"""
+
+print ('hello\'\n\t-word')
